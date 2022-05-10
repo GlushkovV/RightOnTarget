dd8ac42f97ca3e32a2a8dd236b3a4cd643413aaf
Vitaly Glushkov <vit8@live.ru>
2022-05-10 14:42:21 +0400

    Initial Commit

---
+++ RightOnTarget/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RightOnTarget
+//
+//  Created by Vitaly Glushkov on 10.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
